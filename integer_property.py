79b523aacfc78f16406374a9f1a6fc9e88dd807b
mohammed1916 <63586305+mohammed1916@users.noreply.github.com>
2021-05-10 13:31:02 +0530

    Create integer_property.py

---
+++ integer_property.py
@@ -0,0 +1,15 @@
+"""John would like to create a function to check whether given number is
+positive or negative or neutral. Help him to write an independent function
+and pass different inputs to the function and check its behaviour."""
+
+def check(n):
+  if n > 0 :
+    print('Positive')
+  elif n < 0 :
+    print('Negative')
+  else:
+    print('newtral')
+    
+check(-2)
+check(1)
+check(0)
