112bbd81e5b0d71889ae314a38ac45ddd771b4d0
Mohammed <arrahmaan9901@gmail.com>
2021-05-30 15:04:53 +0530

    success

---
+++ python_advanced_concepts/Classes/getter.py
@@ -0,0 +1,27 @@
+"""
+Create a class Student with parameterised constructor name and gender. Create a getter which
+returns name in the format Mr. <<name>> if the gender is Male, Ms. <<name>> if the gender is
+Female.
+"""
+
+class Student:
+    def __init__(self,name,gender):
+        self.gender = gender
+        self.student_name = name
+        
+
+
+    @property
+    def student_name(self):
+        return self.__name
+
+    @student_name.setter
+    def student_name(self,n):
+        if self.gender == "M":
+            self.__name ="Mr." + n
+        else:
+            self.__name ="Ms." + n
+
+s = Student("Mohammed","M")
+print(s.student_name)
+        
