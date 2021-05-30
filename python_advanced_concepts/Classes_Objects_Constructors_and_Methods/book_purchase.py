3df698b1ad0178dce1a055a28d6b2091892960fa
mohammed1916 <63586305+mohammed1916@users.noreply.github.com>
2021-05-30 16:02:14 +0530

    success

---
+++ python_advanced_concepts/Classes_Objects_Constructors_and_Methods/book_purchase.py
@@ -0,0 +1,50 @@
+"""
+Create a class Book with a parameterised constructor then takes name_of_book, author,
+year_of_publish, price_of_book, no_of_copies_available. Create the following methods -
+- order_book(no_of_books) : This method should return price of purchase, if no of
+books is less than or equal to no_of_copies_available. Else, return “No stock”.
+- add_quantity(is_admin, quantity): This method should add quantity of books (2nd
+param) to existing no_of_copies_available if is_admin is True and return “Book
+quantity updated as <<count>>” . If is_admin is False, return “Unauthorised” as
+output.
+
+Sample Input/Output:
+book = Book(“Software Quality Assurance”, “Mr.Jochen”, 1994, 100, 10)
+
+book.order_book(10)            1000                       10*100 = 1000
+book.order_book(1000)          No stock
+book.order_book(1)             100                        1*100 = 100
+book.order_book(43)            No stock
+book.add_quantity(False, 100)  Unauthorized
+book.add_quantity(True, 31)     Book quantity updated as 41
+"""
+
+class Book:
+    def __init__(self,name_of_book, author, year_of_publish, price_of_book, no_of_copies_available):
+        self.name_of_book = name_of_book
+        self.author = author
+        self.year_of_publish = year_of_publish
+        self.price_of_book = price_of_book
+        self.no_of_copies_available = no_of_copies_available
+
+    def order_book(self, no_of_books):
+        if no_of_books <= self.no_of_copies_available:
+            return no_of_books*self.price_of_book
+        else:
+            return "No stock"
+
+    def add_quantity(self, is_admin, quantity):
+        if is_admin:
+            self.no_of_copies_available += quantity
+            return "Book quantity updated as "+ str(self.no_of_copies_available)
+        else:
+            return 'Unauthorised'
+
+book = Book('Software Quality Assurance', 'Mr.Jochen', 1994, 100, 10)
+
+print(book.order_book(10))           
+print(book.order_book(1000))         
+print(book.order_book(1))             
+print(book.order_book(43))
+print(book.add_quantity(False, 100))
+print(book.add_quantity(True, 31))
---
+++ python_advanced_concepts/Classes_Objects_Constructors_and_Methods/bus_ticket_booking.py
@@ -0,0 +1,53 @@
+"""
+Python Classes, Objects, Constructor - I
+
+
+Create a python class called ‘Bus’ which has conductor name, total seats, seats booked, bus unique 
+registration number, driver name as their properties. Create member functions -
+Constructor which takes registration_name, conductor_name, driver_name, total_seats, seats_booked.
+print_bus_details  -> This function should print ALL details of the bus.
+is_seat_available -> This function should return True if a seat available, if not return False.
+book_seat(no_of_seats) -> This function should book a seats if seats available, else return message “Requested no of seats not available”.
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+Example Test Case:
+obj = Bus("TN101", "Joe", "John", 25, 12)
+obj.book_seat(1) => Seats Booked Successfully
+obj.book_seat(2) => Seats Booked Successfully
+obj.book_seat(21) => Requested no of seats not available
+Example Test Case 2:
+obj = Bus("TN102", "Britto",  "Prince", 100, 100)
+obj.book_seat(1) => Requested no of seats not available
+"""
+
+class Bus:
+    def __init__(self,registration_name, conductor_name, driver_name, total_seats, seats_booked):
+        self.registration_name = registration_name
+        self.conductor_name = conductor_name
+        self.driver_name = driver_name
+        self.total_seats = total_seats
+        self.seats_booked = seats_booked
+
+    def print_bus_details(self):
+        print("Bus Details:")
+        print("Register No: ", self.registration_name)
+        print("Conductor name: ", self.conductor_name)
+        print("Driver name: ",self.driver_name)
+        print("Total seats: ", self.total_seats)
+        print("Seats_booked: ",self.seats_booked)
+
+    def is_seat_available(self):
+        return self.seats_booked < self.total_seats
+
+    def book_seat(self,no_of_seats):
+        if self.is_seat_available() and (self.total_seats - self.seats_booked) >=no_of_seats:
+            self.seats_booked += no_of_seats
+            return 'Seats Booked Successfully'
+        else:
+            return 'Requested no of seats not available'
+
+obj = Bus("TN101", "Joe", "John", 25, 12)
+print(obj.book_seat(1) )  
+print(obj.book_seat(2) )
+print(obj.book_seat(21) )
+
+obj.print_bus_details()
